1adc8d58034b007f218f5b24eff4ca9d62aa33e1
sb-portfolio <sibond3@gmail.com>
2023-06-05 14:51:03 +0100

    add auth redirect

--- polka-spot/src/App.js
+++ polka-spot/src/App.js
@@ -4,6 +4,10 @@ import songData from './songData'
 import SearchResults from './Components/SearchResults'
 import Playlist from './Components/Playlist'
 
+const clientId =  '601acf698e384e12b3846478ca604c80'
+const clientSecret = '8c99c97d04a84a658636f8178e3f30f5'
+const redirectUri = 'http://localhost:3000/callback/';
+
 function App() {
 
   const [playList, setPlaylist] = useState([])
@@ -21,41 +25,61 @@ function App() {
     })
   }
 
+
+// const param = window.location.search
+// const urlParams = new URLSearchParams(param);
+//   console.log(urlParams.get('product'))
+
   function removeSongFromPlaylist(id){
     setPlaylist(prevPlaylist => prevPlaylist.filter(track => track.id!==id))
   }
 
   async function getNewSearch(searchParameter){
     console.log(searchParameter)
-//
-    //const response = await fetch("https://swapi.dev/api/people/1")
-    //const response = await fetch("https://accounts.spotify.com/authorize")
-    const response = await fetch("https://accounts.spotify.com/api/token")
 
-    const json = await response.json()
-    console.log(json)
+    let  url = 'https://accounts.spotify.com/authorize';
+    url += '?response_type=token';
+    url += '&client_id=' + clientId;
+    url += '&redirect_uri=' + redirectUri;
+    //Redirect to spotify login
+    window.location.href = url;
+
+
+
+
+    // const response = fetch(url, {
+    //     // headers: {
+    //     //   'Content-Type': 'application/x-www-form-urlencoded'
+    //     // },
+    //     redirect: 'follow',
+    //     method: 'GET',
+    //     mode: 'no-cors'
+    // })
+
+    //const json = await response.json()
+    //console.log(json)
+
+
+    //Get api token
+    // const url = 'https://accounts.spotify.com/api/token';
+    // const response = await fetch(url, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/x-www-form-urlencoded'
+    //     },
+    //     body: 'grant_type=client_credentials&client_id=' + clientId + '&client_secret=' + clientSecret,
+    //     json: true
+    // });
+    // if (response.ok) {
+    //     const jsonResponse = await response.json();
+    //     console.log(jsonResponse);
+    // } else {
+    //     console.log(response.statusText);
+    //     throw new Error(`Request failed! Status code: ${response.status} ${response.statusText}`);
+    // }
 
   }
 
-//   var client_id = 'CLIENT_ID';
-// var redirect_uri = 'http://localhost:8888/callback';
-
-// var app = express();
-
-// app.get('/login', function(req, res) {
-
-//   var state = generateRandomString(16);
-//   var scope = 'user-read-private user-read-email';
-
-//   res.redirect('https://accounts.spotify.com/authorize?' +
-//     querystring.stringify({
-//       response_type: 'code',
-//       client_id: client_id,
-//       scope: scope,
-//       redirect_uri: redirect_uri,
-//       state: state
-//     }));
-// });
 
   function addNewPlaylist(playlistName){
     console.log(playList)
